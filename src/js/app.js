97f54b73cbf6c34a92047869f3ad873c2e85b8f8
Moritz Tietze <moritz.tietze@icloud.com>
2021-03-09 08:53:57 +0100

    switched to bootstrap and added fullscreen ui functions

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,5 @@
+
+
 //p5 init
 const sketch = function (p) {
     p.setup = setup;
@@ -5,7 +7,7 @@ const sketch = function (p) {
     p.windowResized = windowResized;
 };
 
-//event handlers
+//event handler assignment
 document.onload = function (e) {
     new p5(sketch, 'canvas');
 
@@ -13,7 +15,43 @@ document.onload = function (e) {
 };
 document.onkeydown = keyDownDocument
 document.getElementById("rangeScaleFactor").addEventListener("input", inputRangeScaleFactor)
+document.getElementById("btnDownload").addEventListener("click", clickBtnDownload)
+document.getElementById("btnHelp").addEventListener("click", clickBtnHelp)
+document.getElementById("btnInfo").addEventListener("click", clickBtnInfo)
+if (screenfull.isEnabled) {
+	screenfull.on('change', fullscreenChanged);
+}
+
+// UI events
+function fullscreenChanged() {
+    //switch icon on fullscreen switch
+    if(screenfull.isFullscreen) {
+        $("#icnMaximize").addClass("d-none")
+        $("#icnMinimize").removeClass("d-none")
+    } else {
+        $("#icnMinimize").addClass("d-none")
+        $("#icnMaximize").removeClass("d-none")
+    }
+}
+
+function inputRangeScaleFactor(e) {
+    scaleFactor = document.getElementById("rangeScaleFactor").value / 10
+}
+
+function clickBtnDownload(e) {
+
+}
+
+function clickBtnHelp(e) {
+    
+}
+
+function clickBtnInfo(e) {
+    
+}
 
 
 //icon replacing
 feather.replace()
+
+//ui
--- src/js/input.js
+++ src/js/input.js
@@ -39,10 +39,6 @@ function keyDownDocument(e) {
 
 }
 
-function inputRangeScaleFactor(e) {
-    scaleFactor = document.getElementById("rangeScaleFactor").value / 10
-}
-
 function previousBond() {
     do {
         currentBond -= 1
